bb103f1c5bc124ec6c92008ff78b6b92da884279
AJ-froogh <ahmadjawid.froogh456@gmail.com>
2023-03-15 16:06:53 +0000

    Comarison Completed.

--- src/main/java/Comparators.java
+++ src/main/java/Comparators.java
@@ -23,7 +23,7 @@ public class Comparators {
      * @param b the second number to be compared.
      * @return true if a is equal to b, false otherwise.
      */
-    public boolean isEqual(int a, int b){
+    public boolean isEqual(int a, int b){if(a==b){return true;}
         return false;
     }
 
@@ -32,7 +32,7 @@ public class Comparators {
      * @param b the second number to be compared.
      * @return true if a is greater than b, false otherwise.
      */
-    public boolean greaterThan(int a, int b){
+    public boolean greaterThan(int a, int b){if(a>b){return true;}
         return false;
     }
     /**
@@ -40,7 +40,7 @@ public class Comparators {
      * @param b the second number to be compared.
      * @return true if a is less than b, false otherwise.
      */
-    public boolean lessThan(int a, int b){
+    public boolean lessThan(int a, int b){if(a<b){return true;}
         return false;
     }
     /**
@@ -48,16 +48,16 @@ public class Comparators {
      * @param b the second number to be compared.
      * @return true if a is greater than or equal to b, false otherwise.
      */
-    public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+    public boolean greaterThanOrEqualTo(int a, int b){if((a>b)||(a==b)){return true;}else
+        {return false;}
     }
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is less than or equal to b, false otherwise.
      */
-    public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+    public boolean lessThanOrEqualTo(int a, int b){if((a<b)||(a==b)){return true;}else
+        {return false;}
     }
 
     /**
@@ -65,7 +65,7 @@ public class Comparators {
      * @param b the second number to be compared.
      * @return true if a is not equal to b, false otherwise.
      */
-    public boolean notEqualTo(int a, int b){
-        return false;
+    public boolean notEqualTo(int a, int b){if(a!=b){return true;}else
+        {return false;}
     }
 }
